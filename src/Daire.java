a4c4af354302e20cfbffd4bf73d169d1796643f6
mralibatu <alibatuhan404@gmail.com>
2022-06-19 02:45:09 +0300

    first

---
+++ src/Daire.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Daire {
+    public static void main(String[] args) {
+        int yaricap,merkezaci;
+        double alan;
+        Scanner girdi = new Scanner(System.in);
+
+        System.out.println("Yaricapi giriniz : ");
+        yaricap = girdi.nextInt();
+        System.out.println("Merkez aci olcusu girin : ");
+        merkezaci = girdi.nextInt();
+
+        alan = (3.14 * (yaricap * yaricap) * merkezaci)/360;
+
+        System.out.println("Alan : " + alan);
+
+    }
+}
